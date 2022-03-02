735e3028c9725f3e4571384b1c615c7071582607
hamdi bourounia <hamdi.bourounia@sesame.com.tn>
2022-03-02 01:12:48 +0100

    Initial commit

---
+++ src/test/java/com/example/paymentstrike/PaymentStrikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentstrike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentStrikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
